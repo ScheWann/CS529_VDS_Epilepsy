3d124f2df5523a792eb6eb9b941619890f0100f5
ScheWann <92758590+ScheWann@users.noreply.github.com>
2023-11-28 13:12:19 -0600

    back to development mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -229,11 +229,11 @@ const App = () => {
       >
         {collapsed ? (
           <div className="projectTitle">
-            <img src="./brain-svgrepo-com.svg" alt="Your SVG" />
+            <img src="/brain-svgrepo-com.svg" alt="Your SVG" />
           </div>
         ) : (
           <div className="projectTitle">
-            <img src="./brain-svgrepo-com.svg" alt="Your SVG" />
+            <img src="/brain-svgrepo-com.svg" alt="Your SVG" />
             <div style={{ fontSize: 16, padding: 5 }}>EpliepsyBrain</div>
           </div>
         )}
