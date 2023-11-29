be24e3473ce3898eb1e56629a1239a2cb52b5187
ScheWann <92758590+ScheWann@users.noreply.github.com>
2023-11-29 17:50:16 -0600

    add modal(detailed 2D brain view)

--- frontend/src/components/network-viewer/projectionNodeViewer.js
+++ frontend/src/components/network-viewer/projectionNodeViewer.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
-import { Card, Spin } from "antd";
+import { Card, Button, Modal } from "antd";
+import { EyeOutlined } from "@ant-design/icons";
+import { SimilarViewer } from "../similar-viewer/similarViewer";
 
 const colorslist = [
   "#1f77b4",
@@ -19,9 +21,11 @@ export const ProjectionNodeViewer = ({
   brainSvgData,
 }) => {
   const svgRefs = useRef({});
+  const modalSvgRef = useRef(null);
 
   const [svgDimensions, setSvgDimensions] = useState({ width: 0, height: 0 });
   const [selectedRoi, setSelectedRoi] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Create a mapping from ROI to color
   const roiColorMapping = allnetwork.reduce((acc, data, index) => {
@@ -33,18 +37,109 @@ export const ProjectionNodeViewer = ({
     setSelectedRoi(roi);
   };
 
+  const showModal = (roi) => {
+    setSelectedRoi(roi);
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   // Handler to reset selection on outside clicks
   const handleOutsideClick = (event) => {
-    if (selectedRoi && !event.target.closest('.roiBrainCard')) {
+    if (selectedRoi && !event.target.closest(".roiBrainCard")) {
       setSelectedRoi(null);
     }
   };
 
+  // Function to render the ROI graph in the modal
+  const renderRoiGraphInModal = (roi) => {
+    const svgContainer = modalSvgRef.current;
+    if (!svgContainer) return;
+
+    const width = 500; // Adjust modal SVG width as needed
+    const height = 500; // Adjust modal SVG height as needed
+    d3.select(svgContainer).selectAll("svg").remove(); // Clear previous SVG
+
+    const svg = d3
+      .select(svgContainer)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+      const centerX = width / 2;
+      const centerY = height / 2;
+
+      const brainOutline = d3.polygonHull(
+        brainSvgData.map((d) => [d.x, d.y])
+      );
+
+      const xExtent = d3.extent(brainOutline, (d) => d[0]);
+      const yExtent = d3.extent(brainOutline, (d) => d[1]);
+      const outlineWidth = xExtent[1] - xExtent[0];
+      const outlineHeight = yExtent[1] - yExtent[0];
+
+      const scaleX = width / outlineWidth;
+      const scaleY = height / outlineHeight;
+      const scale = Math.min(scaleX, scaleY) * 0.8;
+
+      const translateX = centerX - (xExtent[0] + outlineWidth / 2) * scale;
+      const translateY = centerY - (yExtent[0] + outlineHeight / 2) * scale;
+
+      // Apply translation to each point in the brain outline
+      const translatedOutline = brainOutline.map((point) => [
+        point[0] * scale + translateX,
+        point[1] * scale + translateY,
+      ]);
+
+      // Draw the convex hull as a path
+      svg
+        .append("path")
+        .data([translatedOutline])
+        .attr(
+          "d",
+          d3
+            .line()
+            .x((d) => d[0])
+            .y((d) => d[1])
+        )
+        .attr("stroke", "black")
+        .attr("fill", "none");
+
+      // Draw electrodes for this ROI
+      electrodeScreenPositions.forEach((electrode) => {
+        const position = electrode.label === String(roi) ? true : false;
+        if (position) {
+          const transformedX = electrode.x * scale + translateX * 0.5;
+          const transformedY = electrode.y * scale + 280;
+
+          svg
+            .append("circle")
+            .attr("cx", transformedX)
+            .attr("cy", transformedY)
+            .attr("r", 3)
+            .attr("fill", roiColorMapping[roi] || "blue"); // Use ROI color mapping
+        }
+      });
+  };
+
+  // For showing detailed brain model
+  useEffect(() => {
+    if (isModalOpen && selectedRoi) {
+      renderRoiGraphInModal(selectedRoi);
+    }
+  }, [isModalOpen, selectedRoi]);
+
   // Attach event listener
   useEffect(() => {
-    document.addEventListener('click', handleOutsideClick);
+    document.addEventListener("click", handleOutsideClick);
     return () => {
-      document.removeEventListener('click', handleOutsideClick);
+      document.removeEventListener("click", handleOutsideClick);
     };
   }, [selectedRoi]);
 
@@ -203,7 +298,7 @@ export const ProjectionNodeViewer = ({
                 .append("circle")
                 .attr("cx", transformedX)
                 .attr("cy", transformedY)
-                .attr("r", 3) // Adjust radius as needed
+                .attr("r", 3)
                 .attr("fill", roiColorMapping[roi] || "blue"); // Use ROI color mapping
             }
           });
@@ -226,7 +321,25 @@ export const ProjectionNodeViewer = ({
           <Card
             size="small"
             hoverable
-            title={"ROI" + " " + roi}
+            // title={"ROI" + " " + roi}
+            title={
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <span>{"ROI" + " " + roi}</span>
+                <Button
+                  size="small"
+                  type="primary"
+                  onClick={showModal}
+                  icon={<EyeOutlined />}
+                  ghost
+                />
+              </div>
+            }
             key={roi}
             className="roiBrainCard"
             style={{
@@ -236,7 +349,7 @@ export const ProjectionNodeViewer = ({
               maxHeight: 175,
               border:
                 roi === selectedRoi
-                  ? "2px solid #6495ED"
+                  ? "1px solid #6495ED"
                   : "1px solid rgba(0, 0, 0, 0.125)",
             }}
             onClick={() => selectRoi(roi)}
@@ -251,6 +364,14 @@ export const ProjectionNodeViewer = ({
             ></div>
           </Card>
         ))}
+        <Modal
+          title="Detailed ROI View"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+         <div ref={modalSvgRef}></div> 
+        </Modal>
       </div>
     </Card>
   );
